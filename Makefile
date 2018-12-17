5391366363868f481d4fce4853fdaba6f3c3fb89
darachm <dchmiller@gmail.com>
2018-12-16 17:52:12 -0800

    sketching out pythong script

--- Makefile
+++ Makefile
@@ -4,4 +4,7 @@
 all: 
 	@echo all
 
+.notes.yaml: notes.yaml
+	python3 scripts/intake_notes.py
+	touch .notes.yaml
 
---
+++ scripts/intake_notes.py
@@ -0,0 +1,39 @@
+
+#import yaml
+import argparse
+import os
+#import hash
+#import isoties
+
+if main
+
+    args.argparsenew
+    args.adda command ("-p","parse it",default=False) 
+    args.adda command ("config_file","config file",default="config.notes") 
+    argparse it
+
+    config = []
+    with c as open(args.config_file,"r"):
+        config = yamlread c
+    
+    if not args.p:
+        return 0
+
+    with f as open(config["intake_notes"],"r"):
+
+        this_file = yamlread f
+
+        this_hash = hash(this_file)
+
+        for i in this_file["f"]:
+            this_file["n"] = this_file["n"].sub(i,this_hash+i)
+
+        this_file["f"] = this_file["f"].concat hash in cool way
+
+        this_file["d"] = this_file["d"].parse right
+
+        with f as open(config["notebook_directory"]+"/"+this_hash)
+            print to f the entry again
+
+
+    os.copy(config["template_file"],config["intake_notes"])
