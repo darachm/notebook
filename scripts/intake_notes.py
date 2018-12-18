4921909bb16051857ae5b62764bac6f26fe192a2
darachm <dchmiller@gmail.com>
2018-12-18 15:03:30 -0800

    here's a draft of it

--- scripts/intake_notes.py
+++ scripts/intake_notes.py
@@ -6,6 +6,10 @@ import sys
 import yaml
 import maps
 import hashlib 
+import datetime
+import isodate
+import re 
+import shutil
 #import isoties
 
 if __name__ == '__main__':
@@ -25,24 +29,69 @@ if __name__ == '__main__':
 
     for this_entry in these_entries:
 
-        this_hash_string = (
-            str(this_entry["d"]) + 
-            "".join(this_entry["t"]) +
-            "".join(this_entry["f"]) +
-            "".join(this_entry["n"])
-            )
+        if this_entry["d"] is None or this_entry["t"] is [] or this_entry["n"] is None:
+            raise Exception("You need to have a date, tags, and notes!")
 
-        this_hash_hex = hashlib.blake2b(this_hash_string.encode("utf-8")).hexdigest()
-        print(this_hash_hex)
+        try:
+            this_entry["correction"]
+        except:
+            this_entry["correction"] = ""
 
-#    for i in this_file["f"]:
-#        this_file["n"] = this_file["n"].sub(i,this_hash+i)
-#
-#    this_file["f"] = this_file["f"].concat hash in cool way
-#
-#    this_file["d"] = this_file["d"].parse right
-#
-#    with f as open(config["notebook_directory"]+"/"+this_hash)
-#        print to f the entry again
-#
-#    os.copy(config["template_file"],config["intake_notes"])
+        this_hash_hex = hashlib.sha1(
+                (   str(this_entry["d"]) + 
+                    "".join(this_entry["t"]) +
+                    "".join(this_entry["f"]) +
+                    "".join(this_entry["n"]) +
+                    "".join(this_entry["correction"])
+                    ).encode("utf-8")
+                ).hexdigest()[:10]
+
+        for i, afile in enumerate(this_entry["f"]):
+            if not os.path.isfile(afile):
+                raise Exception("there ain't no file "+afile)
+
+        this_entry["d"] = str(this_entry["d"])
+        if not re.match("^20",this_entry["d"]):
+            this_entry["d"] = "20"+this_entry["d"]
+
+        this_entry["d"] = isodate.parse_date(this_entry["d"])
+
+        entry_dir = config["notebook_directory"]+"/"+this_hash_hex
+        print(entry_dir)
+        os.makedirs(entry_dir,exist_ok=True)
+
+        for i, afile in enumerate(this_entry["f"]):
+            try:
+                shutil.copyfile(
+                    this_entry["f"][i],
+                    entry_dir+"/"+this_hash_hex+"_"+this_entry["f"][i])
+            except:
+                raise Exception("copying "+afile+" failed!")
+            this_entry["n"] = re.sub(afile,this_hash_hex+"_"+afile,this_entry["n"])
+            this_entry["f"][i] = this_hash_hex+"_"+this_entry["f"][i]
+
+        with open(entry_dir+"/"+this_hash_hex+"_notes.txt","w") as notefile:
+            notefile.write(yaml.dump(this_entry))
+            
+
+    os.makedirs(config["trash"],exist_ok=True)
+    for this_entry in these_entries:
+        for i, afile in enumerate(this_entry["f"]):
+            this_real_filename = re.sub(r"^[0-9a-z]{10}_","",this_entry["f"][i])
+            try:
+                shutil.move(
+                    this_real_filename,
+                    config["trash"]+"/"+this_real_filename
+                    )
+            except:
+                pass
+
+    shutil.move(
+        config["intake_file"],
+        config["trash"]+"/"+this_hash_hex+"_notes.txt"
+        )
+
+    shutil.copyfile(
+        config["template_file"],
+        config["intake_file"]
+        )
