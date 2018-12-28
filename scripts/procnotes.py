00d2b353673d04366527f16d95f2503a5a4c1222
darachm <dchmiller@gmail.com>
2018-12-28 12:09:57 -0800

    Fixed a problem with hexhashes getting made before the date was parsed, fixed a problem of hashes getting around, made it so it could query multiple tags for reports.

--- scripts/procnotes.py
+++ scripts/procnotes.py
@@ -51,7 +51,7 @@ I couldn't read the raw notebook file, at all.
 If this is an error, fix it.
 
 If you're just starting up, then you should just make an empty file there 
-(probably at `books/raw_notebook.yaml`).
+(probably at `book/raw_notebook.yaml`).
 """)
     if len(raw_notebook) == 0:
         raw_notebook = {}
@@ -75,18 +75,28 @@ the template to the intake file (probably `notes.yaml`)
         procd_entries = list()
 
         for this_entry in these_entries:
-    
+
             if this_entry["d"] is None or this_entry["t"] is [] or this_entry["n"] is None:
                 raise Exception("\n"+"""
 For a note to be parsed and recorded, you need to have a date, tags, and notes!
 This corresponds to the `d:`, `t:`, and `n:` fields respectively.
 """)
-    
+
             try:
                 this_entry["correction"]
             except:
                 this_entry["correction"] = ""
-    
+
+            for i, afile in enumerate(this_entry["f"]):
+                if not os.path.isfile(afile):
+                    raise Exception("there ain't no file "+afile)
+
+            this_entry["d"] = str(this_entry["d"])
+            if not re.match("^20",this_entry["d"]):
+                this_entry["d"] = "20"+this_entry["d"]
+
+            this_entry["d"] = isodate.parse_date(this_entry["d"])
+
             this_hash_hex = str(this_entry["d"])+"_"+hashlib.sha1(
                     (   str(this_entry["d"]) + 
                         #"".join(this_entry["t"]) +
@@ -95,16 +105,6 @@ This corresponds to the `d:`, `t:`, and `n:` fields respectively.
                         str(this_entry["correction"])
                         ).encode("utf-8")
                     ).hexdigest()[:10]
-    
-            for i, afile in enumerate(this_entry["f"]):
-                if not os.path.isfile(afile):
-                    raise Exception("there ain't no file "+afile)
-    
-            this_entry["d"] = str(this_entry["d"])
-            if not re.match("^20",this_entry["d"]):
-                this_entry["d"] = "20"+this_entry["d"]
-    
-            this_entry["d"] = isodate.parse_date(this_entry["d"])
             this_entry["hash"] = this_hash_hex
 
             procd_entries.append(this_entry)
@@ -116,21 +116,21 @@ This corresponds to the `d:`, `t:`, and `n:` fields respectively.
                   entry_dir = config["notebook_directory"]+"/"+this_entry["hash"]
                   print(entry_dir)
                   os.makedirs(entry_dir,exist_ok=True)
-      
+
                   for i, afile in enumerate(this_entry["f"]):
                       try:
                           shutil.copyfile(
                               this_entry["f"][i],
-                              entry_dir+"/"+this_hash_hex+"_"+this_entry["f"][i])
+                              entry_dir+"/"+this_entry["hash"]+"_"+this_entry["f"][i])
                       except:
                           raise Exception("copying "+afile+" failed!")
-                      this_entry["n"] = re.sub(afile,this_hash_hex+"_"+afile,this_entry["n"])
-                      this_entry["f"][i] = this_hash_hex+"_"+this_entry["f"][i]
-          
-                  with open(entry_dir+"/"+this_hash_hex+"_notes.txt","w") as notefile:
+                      this_entry["n"] = re.sub(afile,this_entry["hash"]+"_"+afile,this_entry["n"])
+                      this_entry["f"][i] = this_entry["hash"]+"_"+this_entry["f"][i]
+
+                  with open(entry_dir+"/"+this_entry["hash"]+"_notes.txt","w") as notefile:
                       notefile.write(yaml.dump(this_entry))
                       raw_notebook[this_entry["hash"]] = this_entry
-      
+
                   os.makedirs(config["trash"],exist_ok=True)
                   for this_entry in these_entries:
                       for i, afile in enumerate(this_entry["f"]):
@@ -142,67 +142,71 @@ This corresponds to the `d:`, `t:`, and `n:` fields respectively.
                                   )
                           except:
                               pass
-              
+
                   shutil.copy(
                       config["intake_file"],
-                      config["trash"]+"/"+this_hash_hex+"_"+
+                      config["trash"]+"/"+this_entry["hash"]+"_"+
                         re.sub(" ","-",str(datetime.datetime.now()))+
                         "_notes.txt"
                       )
-              
+
         elif args.t:
 
             with open("test_report.html","w") as test_report:
 
                 report = dominate.document(title="Test report")
-        
+
                 with report.head:
                     dominate.tags.link(rel='stylesheet',href=config['css'])
-        
+
                 _entrylist = report.add(dominate.tags.ul())
-        
+
                 for this_entry in procd_entries:
-        
+
                     if this_entry["correction"] == "":
-    
+
                         _anitem = _entrylist.add(dominate.tags.li())
                         _anitem += dominate.tags.h3(str(this_entry["d"]))
                         _anitem += dominate.tags.h4(this_entry["hash"])
                         _anitem += dominate.tags.h4(" ".join(this_entry["t"]))
-        
+
                         for j in this_entry["f"]:
                             _anitem += dominate.tags.h4(dominate.tags.a(j,
                                 href=config["notebook_directory"]+"/"+this_entry["hash"]+"/"+j))
-        
+
                         _anitem += dominate.tags.p(dominate.util.raw(markdown.markdown(this_entry["n"])))
-    
+
                     else:
-    
+
                         _anitem = _entrylist.add(dominate.tags.li())
                         _anitem += dominate.tags.h6("--- correction ---")
-    
+
                         _anitem += dominate.tags.h4(this_entry["hash"])
                         _anitem += dominate.tags.h4(" ".join(this_entry["t"]))
-        
+
                         for j in this_entry["f"]:
                             _anitem += dominate.tags.h4(dominate.tags.a(j,
                                 href=config["notebook_directory"]+"/"+this_entry["hash"]+"/"+j))
-        
+
                         _anitem += dominate.tags.p(dominate.util.raw(markdown.markdown(this_entry["n"])))
-    
+
                 test_report.write(report.render())
 
     if args.q:
 
+        query_tags = re.split(",",args.q)
+
         query_result = list()
 
         for each_key in raw_notebook:
-    
+
             each_entry = raw_notebook[each_key]
 
-            if args.q in each_entry['t']:
+            for each_query in query_tags:
 
-                query_result.append(each_entry)
+                if each_query in each_entry['t']:
+
+                    query_result.append(each_entry)
 
         query_result = sorted(query_result, key=lambda x: str(x["d"])+str(x["correction"]))
 
@@ -224,11 +228,11 @@ This corresponds to the `d:`, `t:`, and `n:` fields respectively.
                     _anitem += dominate.tags.h3(str(i["d"]))
                     _anitem += dominate.tags.h4(i["hash"])
                     _anitem += dominate.tags.h4(" ".join(i["t"]))
-    
+
                     for j in i["f"]:
                         _anitem += dominate.tags.h4(dominate.tags.a(j,
                             href=config["notebook_directory"]+"/"+i["hash"]+"/"+j))
-    
+
                     _anitem += dominate.tags.p(dominate.util.raw(markdown.markdown(i["n"])))
 
                 else:
@@ -238,11 +242,11 @@ This corresponds to the `d:`, `t:`, and `n:` fields respectively.
 
                     _anitem += dominate.tags.h4(i["hash"])
                     _anitem += dominate.tags.h4(" ".join(i["t"]))
-    
+
                     for j in i["f"]:
                         _anitem += dominate.tags.h4(dominate.tags.a(j,
                             href=config["notebook_directory"]+"/"+i["hash"]+"/"+j))
-    
+
                     _anitem += dominate.tags.p(dominate.util.raw(markdown.markdown(i["n"])))
 
             f.write(report.render())
